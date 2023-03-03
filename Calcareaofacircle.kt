26a5578023df8c1891faf4159d004ff32da69070
dameeelola <126824652+dameeelola@users.noreply.github.com>
2023-03-03 01:07:58 +0100

    Update Calcareaofacircle.kt

--- Calcareaofacircle.kt
+++ Calcareaofacircle.kt
@@ -5,7 +5,7 @@ fun main(){
     val radius = readln().toDouble()
 
     val area: Double =  kotlin.math.PI * radius * radius
-    val approximatedArea: Double = String.format("%.2f", area).toDouble()
+    val approximatedArea = String.format("%.2f", area)
     println("The area of a circle of radius $radius is $area")
     println("Which is approximated to $approximatedArea")
 
